3716904464ae8eb661e5af76ae6d83ff8391e624
dfc643 <dfc643@126.com>
2019-10-17 21:07:39 +0800

    添加项目文件。

---
+++ wsl-x11run/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wsl_x11run
+{
+    public partial class Form1 : Form
+    {
+        public String[] args;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string BashCommand(string command)
+        {
+
+            ProcessStartInfo procStartInfo = new ProcessStartInfo("bash", "-c 'DISPLAY=:0.0 " + command + "'")
+            {
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            using (Process proc = new Process())
+            {
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+
+                string output = proc.StandardOutput.ReadToEnd();
+
+                if (string.IsNullOrEmpty(output))
+                    output = proc.StandardError.ReadToEnd();
+
+                return output;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+            string cmd = string.Join(" ", args);
+            BashCommand(cmd);
+            Application.Exit();
+        }
+    }
+}
---
+++ wsl-x11run/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wsl_x11run
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 frm1 = new Form1();
+            frm1.args = args;
+            Application.Run(frm1);
+        }
+    }
+}
